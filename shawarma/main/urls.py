8e5284b7885c3384218dd8f7e7d26000f2af57c8
Ваше Имя <you@example.com>
2025-08-14 21:10:11 +0300

    DZ03

--- shawarma/main/urls.py
+++ shawarma/main/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-
+    path('signupuser/', views.signupuser, name='signupuser'),
+    path('loginuser/', views.loginuser, name='loginuser'),
+    path('logoutuser/', views.logoutuser, name='logoutuser'),
 
 ]
