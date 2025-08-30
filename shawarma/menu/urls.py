67f328dda8ed5e539de5b5ea54e85e691fc56464
Ваше Имя <you@example.com>
2025-08-31 00:08:32 +0300

    DZ_0333

--- shawarma/menu/urls.py
+++ shawarma/menu/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.menu, name='menu'),
 
+
+    path('add_to_cart/<int:dish_id>/', views.add_to_cart, name='add_to_cart'),
+    path('clear_cart/', views.clear_cart, name='clear_cart'),
 ]
